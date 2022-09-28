84afc5fccd7a9d0eefa3558564c19b305ee3acaf
Kamalakannan <kamalkuppus@gmail.com>
2022-09-28 11:51:21 +0100

    Updated CRUD

--- src/main/java/com/example/restapidemo/controller/DetailsController.java
+++ src/main/java/com/example/restapidemo/controller/DetailsController.java
@@ -1,7 +1,5 @@
 package com.example.restapidemo.controller;
 
-import java.util.Map;
-
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
 import com.example.restapidemo.entity.Employee;
 import com.example.restapidemo.service.DetailsService;
@@ -22,22 +19,70 @@ public class DetailsController {
 
 	@Autowired
 	private DetailsService detailsService;
-
-	@GetMapping(path = "employee/{id}" ,produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> getEmployeeDetails(@PathVariable(name = "id") String id) {
-//		System.out.println("ID of the student: "+id);
-		String name=detailsService.getStudentName(id);
-		return new ResponseEntity<>(name,HttpStatus.OK);
+	
+	// Create Employee
+	@GetMapping(path = "save/{name}/{rollnumber}/{address}/{mobileNumber}/{pincode}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> saveEmployee(@PathVariable(name = "name") String name,
+											   @PathVariable(name = "rollnumber") String rollnumber,
+											   @PathVariable(name = "address") String address,
+											   @PathVariable(name = "mobileNumber") String mobileNumber,
+											   @PathVariable(name = "pincode") String pincode ){
+		
+		Employee emp = new Employee();
+		emp.setName(name);
+		emp.setRollnumber(rollnumber);
+		emp.setAddress(address);
+		emp.setMobileNumber(mobileNumber);
+		emp.setPincode(pincode);
+		detailsService.saveEmployee(emp);
+		System.out.println("Employee Created: "+emp.getName());
+		java.util.List<Employee> empList = detailsService.getAllEmployee();
+		return new ResponseEntity<>(empList, HttpStatus.OK);
 	}
-	
-	
+
 	// Fetch All employee list from postgress remote database
 	@GetMapping(path="empList", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> getAllEmployees(){
 		java.util.List<Employee> empList = detailsService.getAllEmployee();
-		System.out.println("Employee List: "+empList);
 		return new ResponseEntity<>(empList, HttpStatus.OK);
 	}
 	
+	// Update Employee
+	@GetMapping(path = "update/{name}/{rollnumber}/{address}/{mobileNumber}/{pincode}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> updateEmployee(
+			   @PathVariable(name = "name") String name,
+			   @PathVariable(name = "rollnumber") String rollnumber,
+			   @PathVariable(name = "address") String address,
+			   @PathVariable(name = "mobileNumber") String mobileNumber,
+			   @PathVariable(name = "pincode") String pincode ){
+		System.out.println("Enetered inside: "+rollnumber);
+		Employee emp = detailsService.getEmployee(rollnumber);
+		emp.setName(name);
+		emp.setRollnumber(rollnumber);
+		emp.setAddress(address);
+		emp.setMobileNumber(mobileNumber);
+		emp.setPincode(pincode);
+		System.out.println("Employee RollNo: "+emp.getRollnumber());
+		detailsService.updateEmployee(emp);
+		java.util.List<Employee> empList = detailsService.getAllEmployee();
+		return new ResponseEntity<>(empList, HttpStatus.OK);
+	}
 	
+	// Delete Employee
+	@GetMapping(path="delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> deleteEmployee(@PathVariable(name = "id") String id){
+		detailsService.deleteEmployee(id);
+		java.util.List<Employee> empList = detailsService.getAllEmployee();
+		return new ResponseEntity<>(empList, HttpStatus.OK);
+	}
+
+	
+	@GetMapping(path = "employee/{id}" ,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getEmployeeDetails(@PathVariable(name = "id") String id) {
+    //		System.out.println("ID of the student: "+id);
+		String name=detailsService.getStudentName(id);
+		return new ResponseEntity<>(name,HttpStatus.OK);
+	}
+
+
 }
--- src/main/java/com/example/restapidemo/service/DetailsService.java
+++ src/main/java/com/example/restapidemo/service/DetailsService.java
@@ -1,6 +1,7 @@
 package com.example.restapidemo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,23 @@ public class DetailsService {
 @Autowired
 private EmployeeRepository employeeRepository;	
 	
-	
-public String getStudentName(String id) {
-	
+// Save Employee
+public Employee saveEmployee(Employee emp) {
+	employeeRepository.save(emp);
+	return emp;
+}
+
+// Get Employee
+public Employee getEmployee(String id) {
+	return employeeRepository.findById(id).get();
+}
+
+public Employee updateEmployee(Employee emp) {
+	employeeRepository.save(emp);
+	return emp;
+}
+
+public String getStudentName(String id) {	
 	Employee aEmployee=employeeRepository.findById(id).get();
 	return aEmployee.getName();
 }
@@ -24,6 +39,11 @@ public String getStudentName(String id) {
 public List<Employee> getAllEmployee(){
 	List<Employee> empList = (List<Employee>) employeeRepository.findAll();
 	return empList;
+}
+
+public String deleteEmployee(String id) {
+	employeeRepository.deleteById(id);
+	return "";
 }
 	
 }
